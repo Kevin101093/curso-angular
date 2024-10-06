548d34d1fb0cc06c59cd52b4631aa6f80024cf99
Kevin Duran <kevin101093@hotmail.com>
2024-10-05 19:10:08 -0600

    Proyecto mini-calculadora

--- miniCalculadoraApp/src/app/app.component.ts
+++ miniCalculadoraApp/src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'miniCalculadoraApp';
+  title = 'Mini Calculadora';
+  numero1:number = 0;
+  numero2:number = 0;
+  resultado:number = 0;
+
+  suma():void{
+    this.resultado = this.numero1 + this.numero2;
+  }
+  restar():void{
+    this.resultado = this.numero1 - this.numero2;
+  }
+
 }
+
